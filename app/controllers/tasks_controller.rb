eda8a882f3aabc82b02f5cb0fdb134ff624e4076
Shan Yan <shan.yan@kellogg.northwestern.edu>
2025-02-27 03:01:34 +0000

    class 8

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,30 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    if session["user_id"] != nil 
+      @tasks = Task.where({"user_id"} => session["user_id"])
+    else
+      flash["notice"] = "Log in first!"
+      redirect_to "/login" 
+    end
   end
 
   def create
-    @task = Task.new
-    @task["description"] = params["description"]
-    @task.save
-    redirect_to "/tasks"
+    if session["user_id"] = nil 
+      redirect_to "/tasks"
+    else
+      @task = Task.new
+      @task["description"] = params["description"]
+      @task["user_id"] = session["user_id"]
+      @task.save
+      redirect_to "/tasks"
+    end
   end
 
   def destroy
     @task = Task.find_by({ "id" => params["id"] })
-    @task.destroy
+    if @task["user_id"] == session["user_id"]
+      @task.destroy
+    end 
     redirect_to "/tasks"
   end
 end
--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,12 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+password = "puppies"
+encrypted_pw = BCrypt::Password.create(password)
+puts encrypted_pw
+
 # 2. prepare encrypted string for testing
+puts BCrypt::Password.new(encrypted_pw) == "puppies1"
+
+
 # 3. test secret against prepared encrypted string
